e97e810e6d9602cc7b84e8e7d99e3929dbfe1d1d
Aftab Shaikh <sk.aftab1999@gmail.com>
2026-07-21 00:22:11 +0530

    Solved max consecutive ones 3 with Two Pointers Beating 44%

---
+++ src/main/java/org/leetcode/max_consecutive_ones_III/Solution.java
@@ -0,0 +1,63 @@
+package org.leetcode.max_consecutive_ones_III;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+//    public int longestOnes(int[] nums, int k) {
+//        int longest = 0;
+//        boolean reachedEnd = false;
+//        for (int l = 0; !reachedEnd; l++) {
+//            for (int i = l, j = k; !reachedEnd; i++) {
+//                if (i == nums.length - 1) {
+//                    reachedEnd = true;
+//                }
+//                if (nums[i] == 0) {
+//                    --j;
+//                }
+//                if (j < 0) break;
+//                longest = Math.max(longest, (i - l) + 1);
+//            }
+//        }
+//
+//        return longest;
+//    }
+
+//    public int longestOnes(int[] nums, int k) {
+//        int longest = 0;
+//        int numberOfZeroes = 0;
+//        for (int l = 0, r = 0; r < nums.length; r++) {
+//            if (nums[r] == 0 && numberOfZeroes < k) {
+//                ++numberOfZeroes;
+//            } else if (nums[r] == 0 && numberOfZeroes >= k) {
+//                while (nums[l] != 0) {
+//                    ++l;
+//                }
+//                ++l;
+//            }
+//            longest = Math.max(longest, (r - l) + 1);
+//        }
+//
+//        return longest;
+//    }
+
+    public int longestOnes(int[] nums, int k) {
+        int longest = 0;
+        int numberOfZeroes = 0;
+        ArrayList<Integer> zeroIndexes = new ArrayList<>(nums.length);
+        int currentIndexInZero = 0;
+        for (int l = 0, r = 0; r < nums.length; r++) {
+            if (nums[r] == 0 && numberOfZeroes < k) {
+                zeroIndexes.add(r);
+                ++numberOfZeroes;
+            } else if (nums[r] == 0 && numberOfZeroes >= k) {
+                zeroIndexes.add(r);
+                ++currentIndexInZero;
+                l = zeroIndexes.get(currentIndexInZero);
+            }
+            longest = Math.max(longest, (r - l) + 1);
+        }
+
+        return longest;
+    }
+}
---
+++ src/test/java/org/leetcode/max_consecutive_ones_III/SolutionTest.java
@@ -0,0 +1,15 @@
+package org.leetcode.max_consecutive_ones_III;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void shouldPass() {
+        Assertions.assertEquals(6, new Solution().longestOnes(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2));
+        Assertions.assertEquals(10, new Solution().longestOnes(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3));
+    }
+}
