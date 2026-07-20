c49095b3801cd8041a04c174fb9d8845fb4f4267
Aftab Shaikh <sk.aftab1999@gmail.com>
2026-07-20 10:31:16 +0530

    Solved longest substring without repeating characters using Two Pointers

--- src/main/java/org/leetcode/longest_substring_without_repeating_characters/Solution.java
+++ src/main/java/org/leetcode/longest_substring_without_repeating_characters/Solution.java
@@ -5,21 +5,38 @@ import java.util.Map;
 
 public class Solution {
 
+//    public int lengthOfLongestSubstring(String s) {
+//        Map<Character, Integer> hashTable = new HashMap<>();
+//        int longest = 0;
+//
+//        int currentLongest = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            Integer dupI = hashTable.getOrDefault(c, -1);
+//            if (dupI != -1 && dupI >= (i - currentLongest)) {
+//                currentLongest = i - dupI;
+//            } else {
+//                currentLongest++;
+//            }
+//            longest = Math.max(longest, currentLongest);
+//            hashTable.put(c, i);
+//        }
+//
+//        return longest;
+//    }
+
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> hashTable = new HashMap<>();
         int longest = 0;
 
-        int currentLongest = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            Integer dupI = hashTable.getOrDefault(c, -1);
-            if (dupI != -1 && dupI >= (i - currentLongest)) {
-                currentLongest = i - dupI;
-            } else {
-                currentLongest++;
+        for (int left = 0, right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            Integer foundAtIndex = hashTable.getOrDefault(c, -1);
+            hashTable.put(c, right);
+            if (foundAtIndex != -1 && foundAtIndex >= left) {
+                left = foundAtIndex + 1;
             }
-            longest = Math.max(longest, currentLongest);
-            hashTable.put(c, i);
+            longest = Math.max(longest, (right - left) + 1);
         }
 
         return longest;
